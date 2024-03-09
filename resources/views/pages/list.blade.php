0d0167f91e8d8c79a20216fc4988663b58f2a661
Kamal <magarkamalyb@gmail.com>
2024-03-09 09:07:36 -0800

    first commit

---
+++ resources/views/pages/list.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <li>Nepal</li>
+    <li>India</li>
+    <li>Pakistan</li>
+    <li>Shrilanka</li>
+    <li>Bhutan</li>
+    <li>Maldives</li>
+    <li>Banladesh</li>
+    <li>Afganishtan</li>
+</body>
+</html>
